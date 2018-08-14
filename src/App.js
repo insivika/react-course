9e7a0f3c4c0d80264e736bcfab4ca641a98604e1
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-08-13 21:01:55 -0700

    Added onChange handler for input fields

--- src/App.js
+++ src/App.js
@@ -13,26 +13,45 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked')
     // DONT DO THISthis.state.persons[0].name = 'Maximilian'
-    this.setState({
+    this.setState( {
       persons: [
-        {name: 'Maximilian', age: 28},
+        {name: newName, age: 28},
         {name: 'Manuel', age: 29},
         {name: 'Steph', age: 27}
       ]
-    })
+    } )
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'Stephanie', age: 26}
+      ]
+    } )
   }
 
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App!</h1>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</ Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+       <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+       <Person
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age}/>
+       <Person
+        name={this.state.persons[1].name}
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, "I'm Max!")}
+        changed={this.nameChangedHandler} >My Hobbies: Racing
+       </ Person>
+       <Person
+        name={this.state.persons[2].name}
+        age={this.state.persons[2].age}/>
       </div>
     );
   }
